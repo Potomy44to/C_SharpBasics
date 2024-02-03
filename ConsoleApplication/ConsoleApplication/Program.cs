9c4935833f5b25f5e0897dd770213138acebc577
Potomy44to <s.khlestunov@gmail.com>
2024-02-03 14:56:41 +0300

    Анкета с вопросами

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,6 +1,9 @@
-﻿string myName;
-myName = "Sergey";
+﻿string myName = "Sergey";
+byte myAge = 31;
+bool havePet = true;
+double myShoeSize = 44.5;
 
-Console.WriteLine(myName);
-
-Console.ReadKey();
+Console.WriteLine("My name is {0}", myName);
+Console.WriteLine("My age is {0}", myAge);
+Console.WriteLine("Do you have a pet? {0}", havePet);
+Console.WriteLine("My shoe size is {0}", myShoeSize);
